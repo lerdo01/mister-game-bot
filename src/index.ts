eb4cd59be6e166f1c3efe9d45a305df3d205610c
lbass <104320517+lerdo01@users.noreply.github.com>
2024-05-28 23:07:56 -0300

    fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,3 @@ const client = new ExtendedClient();
 client.start();
 
 export { client, config }
-
-fs.readdirSync(path.join(__dirname, "commands")).forEach(local => {
-    fs.readdirSync(path.join(__dirname, "commands", local))
-    .filter(fileName => fileName.endsWith(".ts") || fileName.endsWith(".js"))
-    .forEach(fileName => {
-console.log(fileName)
-    })
-})
